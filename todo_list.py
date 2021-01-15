730a6f06750df9e824f246dca16ec21ae1393345
Jamshid <mamatovjamshid506@gmail.com>
2021-01-15 23:19:20 +0500

    this function removes the tasks within the todo list

--- todo_list.py
+++ todo_list.py
@@ -7,6 +7,14 @@ def creat_list(lst):
     
     print(f"list name = {name}\ntasks\n{task}")
 
+    print("Do you remove the task -> yes or no = ")
+    remove=input()
+    if remove=="yes":
+        
+        task_remove=remove_task(task)
+        task=task_remove
+        print(task_remove)
+
 
 
 def add_task(add_lst):
@@ -29,5 +37,14 @@ def add_task(add_lst):
     return add_lst
 
 
+def remove_task(tasks):
+    task_remove=[]
+    for task in tasks:
+        print(f"{task}  remove this task ->yes or no = ",end="")
+        remove=input()
+        if remove!='yes':
+            task_remove.append(task)
+
+    return  task_remove
 
 creat_list(todo_list)       
