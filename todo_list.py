cb5696ab5f0ca866dbe1f698704f9f5194bdb64f
Jamshid <mamatovjamshid506@gmail.com>
2021-01-15 23:16:39 +0500

    the function adds a task to the todo list   and called inside the creat function

--- todo_list.py
+++ todo_list.py
@@ -3,9 +3,30 @@ todo_list=[]
 def creat_list(lst):
     print('Enter a list name = ',end="")
     name=input()
+    task=add_task(lst)
+    
+    print(f"list name = {name}\ntasks\n{task}")
 
 
 
+def add_task(add_lst):
+    add=True
+    
+    
+    while add:
+        task={}
+        print("write the task = ",end="")
+        task_text=input()
+        task['text']=task_text
+        task['state']=False
+        add_lst.append(task)
+        
+        print('Do you add a new task ->yes or no = ',end="")
+        add=input()
+        if add=='no':
+            add=False
+
+    return add_lst
 
 
 
