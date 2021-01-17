421600644d4f2a3a0d7fb431dd0d570a931b976d
Jamshid <mamatovjamshid506@gmail.com>
2021-01-17 17:35:13 +0500

    added docstring to function

--- todo_list.py
+++ todo_list.py
@@ -1,111 +1,79 @@
 todo_list=[]
+def creat_list():
+    """creat list
+    parametrs:
+        none
+        there is a global variable
+    return: 
+        none    """
+    global todo_list
 
-def creat_list(lst):
-    """this function takes a single value and this function is the main function and 
-    it combines functions such as task creation task removal modification tasks input tasks"""
 
-    print('Enter a list name = ',end="")
-    name=input()
-    task=add_task(lst)
+def add_task():
+    """task add in list
+    parametrs:
+        none
+        there is global variable
+    return:
+        list->global variable"""
     
-    print(f"list name = {name}\ntasks\n{task}")
-
-    print("Do you remove the task -> yes or no = ")
-    remove=input()
-    if remove=="yes":
-        
-        task_remove=remove_task(task)
-        task=task_remove
-        print(task_remove)
-
-    print("Do you update the task -> yes or no = ")
-    update=input()
-    if update=="yes":
-        
-        task_update=update_task(task)
-        task=task_update
-        print(task_update)   
-
-    print("Do you checking the task -> yes or no = ")
-    checking=input()
-    if checking=="yes":
-        
-        task_checking=checking_task(task)
-        
-        print(task_checking) 
-
-    print("Do you clear the task -> yes or no = ")
-    clear=input()
-    if clear=="yes":
-
-        task_clear=clear_task(task)
-        task=task_clear
-        print(task)         
-
-
-
-def add_task(add_lst):
-    """this function takes a single list and 
-    adds tasks to that list returns the state and text state and the task list"""
-
-    add=True
+    task={}
     
+    task['text']="write"
+    task['state']=False
+    todo_list.append(task)
+
+    task={}
+    task['text']="read"
+    task['state']=False
+    todo_list.append(task)
+
+    task={}
+    task['text']="listen"
+    task['state']=False
+    todo_list.append(task)
+
     
-    while add:
-        task={}
-        print("write the task = ",end="")
-        task_text=input()
-        task['text']=task_text
-        task['state']=False
-        add_lst.append(task)
-        
-        print('Do you add a new task ->yes or no = ',end="")
-        add=input()
-        if add=='no':
-            add=False
 
-    return add_lst
+    return todo_list
 
 
-def remove_task(tasks):
-    """this function takes a single list and 
-    removes the task you specified in that list and returns the list"""
-    task_remove=[]
-    for task in tasks:
-        print(f"{task}  remove this task ->yes or no = ",end="")
-        remove=input()
-        if remove!='yes':
-            task_remove.append(task)
-
-    return  task_remove
-
-def update_task(tasks):
-    """this function accepts the list and 
-    updates and returns the list to the task you want"""
+def remove_task():
+    """todo removes the first task of the list
+        parameters:
+        none
+        there is global variable
+        return:
+        list->global variable"""
     
-    for task in tasks:
-        print(f"{task}  update this task ->yes or no = ",end="")
-        update=input()
-        if update=="yes":
-            print(f"{task['text']} enter new text if you change = ",end="")
-            new_text=input()
-            if new_text!="":
-                task['text']=new_text
+    todo_list.pop(0)
 
-            print(f"{task['state']} enter new text if you change False or True = ",end="")
+    return  todo_list
 
-            new_state=input()
-            if new_state!="":
-                task['state']=bool(new_state)
-    return tasks
+def update_task():
+    """updated task of todo list
+    parameters:
+    none 
+    code has global variable
+    return:
+    list->global variable   """
+    
+    todo_list[1]['text']='speak'
+    todo_list[0]['state']=True
 
-def checking_task(tasks):
-    """   this function accepts the list and 
-    checks the tasks do or donot"""
+    return todo_list
+
+def checking_task():
+    """   checks todo list {do} or {donot}
+    parameters:
+    none 
+    code has global variable
+    return:
+    dict->information about {do} or {donot}"""
     do=0
     do_not=0
     task_chek={}
-    for task in tasks:
+    for task in todo_list:
         if task['state']==True:
             do+=1
         else:
@@ -114,11 +82,14 @@ def checking_task(tasks):
     task_chek['do_not']=do_not
     return  task_chek
 
-def clear_task(tasks) :
-    """   this function accepts the list and 
-    clears the list then returns an empty list"""
+def clear_task() :
+    """clears todo list
+    parameters:
+    none 
+    code has global variable   
+    return:
+    empty list->global variable"""
 
-    tasks.clear()
-    return tasks
+    todo_list.clear()
+    return todo_list
 
-creat_list(todo_list)       
